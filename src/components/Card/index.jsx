2ab65d61c496adbd557541357fffb963676c443f
arthur-arend <arthur.nox@gmail.com>
2021-07-17 19:25:34 -0300

    Estrutura básica do projeto criada.

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './styles.scss';
+
+function Card() {
+  return <div />;
+}
+
+export default Card;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles.scss';
+
+function Header() {
+  return (
+      <header>
+          <h1>Breweries</h1>
+          <h2>A breweries list by Open Brewery DB</h2>
+      </header>
+  );
+}
+
+export default Header;
